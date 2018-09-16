23c8e3a056468dabdfdde5d5d52d240a44d2fdb2
projectxplano <projectx.plano@gmail.com>
2018-09-16 12:08:00 -0500

    restructure-react

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) =>{
+    let buttonClass= '';
+    const styleClasses = [];
+    if(props.persons.length <= 2){
+      styleClasses.push(classes.red);
+    }
+    if(props.persons.length <= 1){
+      styleClasses.push(classes.bold);
+    }
+    if(props.showPersons){
+        buttonClass = classes.Red; 
+    }
+    return (
+        <div className={classes.Cockpit}>              
+        <h1>My First React App</h1>
+        <p className={styleClasses.join(' ')}> "It is working" </p>  
+        <button className={buttonClass} onClick={props.clicked}>Toggle Persons</button> 
+    </div>
+    );
+
+};
+
+export default cockpit;
---
+++ src/containers/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+
+
+class App extends Component {
+  state ={
+   persons : [{id: 'wew82145', name: 'Evan', age: 15},
+    {id:'yrw44882', name: 'Ivan', age: 18},
+    {id:'uew83421', name: 'Tom', age: 14}
+  ],
+  showPersons: false
+  }
+  handleClick = (newName, newAge, index) =>{
+    const newPerson = {name: newName, age: newAge};  
+    const newPersons = [...this.state.persons ];
+    newPersons[index] = newPerson;
+    
+    this.setState({persons : newPersons});
+
+  }
+
+  handleDelete = (index) => {    
+    const newPersons = this.state.persons.slice();   
+    newPersons.splice(index, 1)
+    this.setState({persons: newPersons})
+  }
+
+  handleToggle = () =>{   
+    const showingPersons = this.state.showPersons;
+    this.setState({
+      showPersons: !showingPersons
+    })
+  }
+
+  findIndexById = (id) =>{
+    return this.state.persons.findIndex(
+      p => {return p.id === id}
+      );
+  }
+
+  handleChange = (event, id) =>{
+    //clone person array in state
+    const newPersons = [...this.state.persons ];
+    newPersons[this.findIndexById(id)].name = event.target.value;
+    this.setState({persons : newPersons});
+  }
+
+ 
+  render() {
+    let persons = null;
+    if ( this.state.showPersons ) {
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.handleDelete}
+        changed={this.handleChange} />;
+    }  
+
+    return (      
+      <div className={classes.App}>       
+          <Cockpit 
+          showPersons={this.state.showPersons}
+          persons= {this.state.persons}
+          clicked= {this.handleToggle} 
+          />
+          {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
